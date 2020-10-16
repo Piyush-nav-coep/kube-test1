43da18c71e95684e461708dad3a676f4adfb6fb3
Piyush-nav-coep <navagireps16.extc@coep.ac.in>
2020-10-17 00:45:25 +0530

    lets deploy

--- launch.py
+++ launch.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "lets deploy 1st application!"
+    return "lets deploy 2nd application!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
