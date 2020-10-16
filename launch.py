1d18c3c2b2629a33e261d890c611268fe07b8e67
Piyush-nav-coep <navagireps16.extc@coep.ac.in>
2020-10-17 00:28:11 +0530

    lets deploy

--- launch.py
+++ launch.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Python kubernetes test!"
+    return "lets deploy 1st application!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
